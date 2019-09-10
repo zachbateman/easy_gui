81f6f915c34a9422f72fe2ed7f2ceced5bfa8111
Zach Bateman <zkbateman@gmail.com>
2019-09-10 15:10:33 -0500

    Update plotting test

--- tests/test_plotter.py
+++ tests/test_plotter.py
@@ -2,6 +2,8 @@ import unittest
 import sys
 sys.path.insert(1, '..')
 import easy_gui
+from matplotlib.figure import Figure
+import time
 
 data = [(1, 1), (2, 3), (3, 5), (4, 4), (5, 7), (6, 7)]
 
@@ -18,7 +20,21 @@ class TestPlotterGUI(unittest.TestCase):
         self.gui.sections['controls'].add_widget(type='button', text='Line Plot', command_func=lambda e: print('Button1 working!'))
 
         self.gui.add_section('display')
-        self.gui.sections['display'].add_widget(type='matplotlib')
+        plot = self.gui.sections['display'].add_widget(type='matplotlib', return_widget=True)
+
+
+        fig = Figure(figsize=(4,3), dpi=100)
+        ax = fig.add_subplot(111)
+        ax.scatter([t[0] for t in data], [t[1] for t in data])
+        plot.draw_plot(mpl_figure=fig)
+
+        time.sleep(2)
+
+        fig = Figure(figsize=(4,3), dpi=100)
+        ax = fig.add_subplot(111)
+        ax.scatter([t[1] for t in data], [t[0] for t in data])
+        plot.draw_plot(mpl_figure=fig)
+
 
         self.gui.mainloop()
         self.assertTrue(True)
